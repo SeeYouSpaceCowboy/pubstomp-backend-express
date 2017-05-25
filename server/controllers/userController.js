eb1e124c0960eb35d9f084a3bbea5dd683d3f836
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-05-25 14:16:33 -0400

    Added new endpoints to database for user.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,8 +26,32 @@ userController.create = (request, response) => {
   })
 }
 
+userController.showAll = (request, response) => {
+  db.User.find({}).then((users) => {
+    return response.status(200).json({
+      success: true,
+      data: users
+    })
+  }).catch((err) => {
+    return response.status(500).json({
+      message: err
+    })
+  })
+}
+
 userController.show = (request, response) => {
-  
+  const { email } = request.body
+
+  db.User.findOne({ email }).then((user) => {
+    return response.status(200).json({
+      success: true,
+      data: user
+    })
+  }).catch((err) => {
+    return response.status(500).json({
+      message: err
+    })
+  })
 }
 
 export default userController
